9aa9c45c2f51a052c5640e3f832b09028be9076d
Thiago Benevide <thiagobenevide@live.com>
2022-07-30 11:31:23 -0300

    Codigo
    Codigos do Curso da Udemy de Java  com contexto de toString, swithCase e funcionalidades extras de informações

---
+++ javaCursoUdemy/course/src/course/Program02.java
@@ -0,0 +1,11 @@
+package course;
+
+public class Program02 {
+	public static void main(String[] args) {
+		String s = "potato apple lemon";
+		String[] vetor = s.split(" ");
+		System.out.println(vetor[0]);
+		System.out.println(vetor[1]);
+		System.out.println(vetor[2]);
+	}
+}
