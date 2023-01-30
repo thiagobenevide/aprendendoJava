586c6a6fd7b0c92f8a9d41712ba4dbae494f2184
Thiago Benevide <thiagobenevide@live.com>
2023-01-29 22:57:55 -0300

    Solucionando lista 9 com MVC

--- testesSpringLayout/br.edu.mpoo.listaIX.ThiagoBenevide/questao2/controller/TelaControllerGeral.java
+++ testesSpringLayout/br.edu.mpoo.listaIX.ThiagoBenevide/questao2/controller/TelaControllerGeral.java
@@ -1,7 +1,117 @@
 package controller;
 
-public class TelaControllerGeral {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+
+import view.TelaCadastro;
+
+public class TelaControllerGeral implements MouseListener, CaretListener, FocusListener, KeyListener, ActionListener{
+	TelaCadastro telaCadastro;
+	boolean status;
+	
+	public TelaControllerGeral(TelaCadastro telaCadastro) {
+		this.telaCadastro = telaCadastro;
+		control(telaCadastro);
+	}
 	
 	
+	private void control(TelaCadastro telaCadastrar) {
+		telaCadastro.getMenuSair().addMouseListener(this);
+		telaCadastro.getClienteItem().addActionListener(this);
+		telaCadastro.getAdicionarButton().addActionListener(this);
+		
+	}
+
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void focusGained(FocusEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void focusLost(FocusEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void caretUpdate(CaretEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- testesSpringLayout/br.edu.mpoo.listaIX.ThiagoBenevide/questao2/view/TelaCadastro.java
+++ testesSpringLayout/br.edu.mpoo.listaIX.ThiagoBenevide/questao2/view/TelaCadastro.java
@@ -2,8 +2,11 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -11,18 +14,26 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+import javax.swing.text.MaskFormatter;
+
+import model.SpringUtilities;
+
 
 public class TelaCadastro extends JFrame{
 	JLabel clienteLabel, nomeLabel, cpfLabel, desejaSairLabel;
-	JTextField nomeField, cpfField;
+	JTextField nomeField;
+	JFormattedTextField  cpfField;
 	JButton adicionarButton, simButton, naoButton;
 	JMenuBar menuBarra;
 	JMenu menuCadastrar, menuSair;
 	JMenuItem clienteItem;
 	
+	GridBagConstraints gbc = new GridBagConstraints();
+	
 	public TelaCadastro() {
 		
-		setSize(400,400);
+		setSize(300,200);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setLayout(new BorderLayout());
@@ -36,6 +47,7 @@ public class TelaCadastro extends JFrame{
 		menuCadastrar = new JMenu("Cadastrar");
 		menuSair = new JMenu("Sair");
 		clienteItem = new JMenuItem("Cliente");
+		clienteItem.setAccelerator();
 		
 		
 		menuBarra.add(menuCadastrar);
@@ -44,29 +56,115 @@ public class TelaCadastro extends JFrame{
 		
 		/*Tela Cadastro*/
 		
-		JPanel principal = new JPanel(new BorderLayout());
-		
+		JPanel panelPrincipal = new JPanel(new SpringLayout());
+		JPanel panelCabecalho = new JPanel(new GridBagLayout());
+		/*Configura o cabeçalho*/
 		clienteLabel = new JLabel("Cliente");
-		clienteLabel.setFont(new Font("Arial", Font.BOLD,20));
+		clienteLabel.setFont(new Font("Arial", Font.BOLD,30));
+		gbc.gridx = 2;
+		gbc.gridy = 2;
+		
+		/*Configura o principal*/
+		
 		nomeLabel = new JLabel("Nome");
-		cpfLabel = new JLabel("Cpf");
-		
-		principal.add(clienteLabel, setLayout());
-		
-		
-		adicionarButton = new JButton("Adicionar");
-		
-		add(principal);
-		
-		
-		
-		
 
+		
+		nomeField = new JTextField("Nome completo");
+		try {
+			cpfLabel = new JLabel("Cpf");
+			cpfField = new JFormattedTextField(new MaskFormatter("###.###.###-##"));
+			cpfField.setColumns(10);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
+		
+		panelCabecalho.add(clienteLabel, gbc);
+		
+		panelPrincipal.add(nomeLabel);
+		panelPrincipal.add(nomeField);
+		panelPrincipal.add(cpfLabel);
+		panelPrincipal.add(cpfField);
+		
+		SpringUtilities.makeCompactGrid(panelPrincipal,2,2,6,6,6,6);
+		
+		
+		/*Configura o botão final*/
+		
+		JPanel panelButton = new JPanel(new GridBagLayout());		
+		adicionarButton = new JButton("Adicionar");
+		gbc.gridx = 2;
+		gbc.gridy = 2;
+		panelButton.add(adicionarButton, gbc);
+
+		add(panelCabecalho, BorderLayout.NORTH);
+		add(panelPrincipal, BorderLayout.CENTER);
+		add(panelButton,BorderLayout.PAGE_END);
+		
+		panelCabecalho.setVisible(false);
+		panelPrincipal.setVisible(false);
+		panelButton.setVisible(false);
+		
 		
 		setVisible(true);
 		
 		
 	}
+
+	public JLabel getClienteLabel() {
+		return clienteLabel;
+	}
+
+	public JLabel getNomeLabel() {
+		return nomeLabel;
+	}
+
+	public JLabel getCpfLabel() {
+		return cpfLabel;
+	}
+
+	public JLabel getDesejaSairLabel() {
+		return desejaSairLabel;
+	}
+
+	public JTextField getNomeField() {
+		return nomeField;
+	}
+
+	public JFormattedTextField getCpfField() {
+		return cpfField;
+	}
+
+	public JButton getAdicionarButton() {
+		return adicionarButton;
+	}
+
+	public JButton getSimButton() {
+		return simButton;
+	}
+
+	public JButton getNaoButton() {
+		return naoButton;
+	}
+
+	public JMenuBar getMenuBarra() {
+		return menuBarra;
+	}
+
+	public JMenu getMenuCadastrar() {
+		return menuCadastrar;
+	}
+
+	public JMenu getMenuSair() {
+		return menuSair;
+	}
+
+	public JMenuItem getClienteItem() {
+		return clienteItem;
+	}
+	
+	
 	
 	
 }
